bdd865bca3298774ca84eb3f025f05680763f938
Dylan Fennelly <dylanantfennelly@gmail.com>
2022-12-30 17:17:52 +0000

    LoginPage component + page created, route added to index

--- labMoviesApp/src/index.js
+++ labMoviesApp/src/index.js
@@ -26,6 +26,7 @@ import FantasyMoviesPage from "./pages/fantasyMoviesPage"
 import FantasyMoviesContextProvider from "./contexts/fantasyMoviesContext";
 import AddFantasyMoviePage from "./pages/addFantasyMoviePage";
 import FantasyMovieDetailsPage from "./pages/fantasyMovieDetailsPage";
+import LoginPage from "./pages/loginPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -66,6 +67,7 @@ const App = () => {
                   <Route path="/movies/fantasy" element={<FantasyMoviesPage />} />
                   <Route path="/movies/fantasy/new" element={<AddFantasyMoviePage />} />
                   <Route path="/movies/fantasy/:id" element={<FantasyMovieDetailsPage />} />
+                  <Route path="/login" element={<LoginPage />} />
                 </Routes>
               </FantasyMoviesContextProvider>
             </ActorsContextProvider>
