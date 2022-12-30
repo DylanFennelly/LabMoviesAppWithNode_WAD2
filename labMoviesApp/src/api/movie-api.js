473c5653684bad23390bca5aa8cf926252045da9
Dylan Fennelly <dylanantfennelly@gmail.com>
2022-12-30 17:09:46 +0000

    api endpoints and authContext

---
+++ labMoviesApp/src/api/movie-api.js
@@ -0,0 +1,20 @@
+//interaction with backend api
+export const login = (username, password) => {
+    return fetch('/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
+
+export const signup = (username, password) => {
+    return fetch('/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
